3174fe29d55f306d22fb613310ad708b946c74e0
drewrichardson <drewrichardson2@gmail.com>
2021-02-19 11:10:40 -0700

    updating port, logging IP

--- serveStats.js
+++ serveStats.js
@@ -7,10 +7,12 @@ const cpu = require('./cpu.js')
 const mem = require('./mem.js')
 const os = require('os');
 const { get } = require('http');
-
+const PORT = 3000
+let ip = ""
 app.set('views', __dirname + '/');
 app.set('view engine', 'pug');
 app.get('/', function(req, res) {
+    ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
     res.render('index.pug');
 });
 let data = {}
@@ -24,12 +26,12 @@ setInterval(() => {
 }, 3000);
 
 io.sockets.on('connection', function (sockets) {
-    console.log('client connected');       
+    console.log('client connected', ip);       
     sockets.on('disconnect', function() {
-        console.log('client disconnected');
+        console.log('client disconnected', ip);
     });  
 });
 
-http.listen(3001, function(){
-  console.log('listening on *:3001');
+http.listen(PORT, function(){
+  console.log('listening on', PORT);
 });
